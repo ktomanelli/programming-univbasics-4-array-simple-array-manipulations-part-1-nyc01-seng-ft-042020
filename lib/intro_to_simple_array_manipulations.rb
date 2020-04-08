b2a090eecc0a2881a8ac477bfb6ad526d01964f4
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-08 19:09:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,20 +1,24 @@
 def using_push(arr, str)
   arr.push(str)
+  arr
 end
 
 def using_unshift(arr, str)
   arr.unshift(str)
+  arr
 end
 
 def using_pop(arr, str)
-  ele = arr.pop(str)
-  ele
+  arr.pop(str)
+  arr
 end
 
 def pop_with_args(arr,num=2)
   num.times(arr.pop())
+  arr
 end
 
 def shift_with_args(arr,num=2)
   num.times(arr.shift())
+  arr
 end
